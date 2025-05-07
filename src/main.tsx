d1490ab1a83d47cdeadf00bbcf023c6244507920
shouryalala <shouryalala@gmail.com>
2025-05-06 21:03:57 -0700

    update: added Posthog

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,19 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
+import { PostHogProvider} from 'posthog-js/react'
+
+const options = {
+  api_host: import.meta.env.VITE_PUBLIC_POSTHOG_HOST,
+}
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <PostHogProvider 
+      apiKey={import.meta.env.VITE_PUBLIC_POSTHOG_KEY}
+      options={options}
+    >
+      <App />
+    </PostHogProvider>
   </React.StrictMode>
 );
